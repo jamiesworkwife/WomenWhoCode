79933eb0d8543eeecdcbf895d3211fe7e2f6dc1d
laurasimonetto <62660327+laurasimonetto@users.noreply.github.com>
2020-05-13 13:16:13 +0200

    added Lesson 3 & 4

---
+++ Lesson4/js/scripts.js
@@ -0,0 +1,39 @@
+//var urlParams = new URLSearchParams(window.location.search);
+//var name = urlParams.get('name') ? urlParams.get('name') : 'not set.';
+
+
+
+// $('span.name').text(name);
+
+
+// let nonsense ="jsfasjfsafjjkk";
+// let one = 1; // float
+// let onepointtone = 1.1; // float
+// let booltrue = true; // boolean
+
+let my_list = [ 
+   'Laura',
+   'Jamie',
+   'Tish',
+   'Lisa'
+   'Marleen'
+ ];
+
+for (let i = 0; i < my_list.length; i++) {
+console.log(my_list[i]);
+}
+
+// console.log(my_list);
+// console.log(my_list[1]);
+
+// let my_object = {
+	//'name': 'Laura',
+	//'Role': 'Office Manager, Work Wife',
+	//'weekly_salary': 10000,
+	//'days_off': 1.5,
+	//'most_important': true
+
+//};
+
+//console.log(my_object);
+//console.log(my_object.weekly_salary);
